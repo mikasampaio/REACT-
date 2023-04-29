e141f5a4e2da0f3f6563fb01c075292e3eb59f74
mikasampaio <90917379+mikasampaio@users.noreply.github.com>
2023-04-28 21:11:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# REACT-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-p18w7c)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { H1 } from './styles.js'
+
+export default function App() {
+  return (
+    <div>
+      <H1>Hello StackBlitz!</H1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
