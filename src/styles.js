9204659bc95977e0816a721923635d04a9bf4583
mikasampaio <90917379+mikasampaio@users.noreply.github.com>
2023-04-29 11:59:44 -0300

    1 file modified

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,26 @@
 import styled from 'styled-components'
 
+export const Container = styled.div `
+
+`
+
+export const Image = styled.img `
+
+`
+
+export const ContainerItens = styled.div `
+
+`
+
 export const H1 = styled.h1 `
 color: blue;
 `
+export const Label = styled.p `
 
+`
+
+export const Input = styled.input `
+
+`
 
 
